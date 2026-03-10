131d6f04161fc058be16cffae635eed9757a957f
DDXDB <984400286@qq.com>
2026-03-10 18:46:25 +0800

    更新

---
+++ app.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const cors = require('cors')
+const express = require('express')
+const compression = require('compression')
+
+const routers = {}
+
+const app = express()
+const PORT = process.env.PORT || 5000
+const REPO_URL = 'https://raw.githubusercontent.com/DDXDB/MGG-AGT-ZH'
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next()
+})
+
+app.use(cors())
+app.use(compression())
+app.use(express.json())
+
+Object.entries(routers).forEach(([path, router]) => {
+    app.use(`/${path}`, router)
+})
+
+app.all('/', (req, res) => res.redirect('https://github.com/DDXDB/MGG-AGT-ZH'))
+
+Array.from(['names', 'titles', 'scenes']).forEach(cls => {
+    app.get(`/translation/${cls}/*`, (req, res) => {
+        const filePath = path.join(__dirname, 'translation', `${cls}/${req.params[0]}`)
+        res.sendFile(filePath, err => err && res.sendStatus(404))
+        // res.redirect(`${REPO_URL}/refs/heads/main/translation/${cls}/${req.params[0]}`)
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
